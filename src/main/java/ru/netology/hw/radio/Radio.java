3fa4463c38303f3c4eef075d9258f988401001d6
MG <gmelina69@gmail.com>
2026-03-14 18:05:10 +0400

    Добавить аннотации Lombok для сокращения кода
    1. Добавление аннотации @Data, который генерирует для каждого значения методы getter(), setter()
    2. Добавление аннотации @NoArgsConstructor, генерирующая конструктор по умолчанию

--- src/main/java/ru/netology/hw/radio/Radio.java
+++ src/main/java/ru/netology/hw/radio/Radio.java
@@ -1,31 +1,24 @@
 package ru.netology.hw.radio;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
 public class Radio {
-    private int totalStations;
-    private int maxStation;
+    private int totalStations = 10;
+    private int maxStation = 9;
     private int minStation = 0;
     private int currentStationNum = minStation;
     private int maxVolume = 100;
     private int minVolume = 0;
     private int currentVolume = minVolume;
 
-    public Radio() {
-        this.totalStations = 10;
-        this.maxStation = 9;
-    }
-
     public Radio(int totalStations) {
         this.totalStations = totalStations;
         this.maxStation = totalStations - 1;
         this.currentStationNum = minStation;
-    }
-
-    public int getTotalStations() {
-        return totalStations;
-    }
-
-    public int getCurrentStationNum() {
-        return currentStationNum;
     }
 
     public void next() {
@@ -49,14 +42,6 @@ public class Radio {
             return;
         }
         currentStationNum = newCurrentStationNum;
-    }
-
-    public int getCurrentVolume() {
-        return currentVolume;
-    }
-
-    public void setCurrentVolume(int newCurrentVolume) {
-        currentVolume = newCurrentVolume;
     }
 
     public void increaseVolume() {
